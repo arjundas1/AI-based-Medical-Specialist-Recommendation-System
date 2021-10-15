036f67c12f518e285423bf687d70d066acca1be0
Arjun Das <72820515+arjundas1@users.noreply.github.com>
2021-10-15 08:34:25 +0530

    Update Visualization.py

--- Machine Learning/Visualization.py
+++ Machine Learning/Visualization.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-df1 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Symptom.csv')
-df2 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Symptom Description.csv')
-df3 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Symptom Precaution.csv')
-df4 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Symptom Severity.csv')
+df1 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Medical/Symptom.csv')
+df2 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Medical/Symptom Description.csv')
+df3 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Medical/Symptom Precaution.csv')
+df4 = pd.read_csv('C:/Users/Arjun/Downloads/Dataset/Medical/Symptom Severity.csv')
 
 sns.set()
 vis1 = plt.hist(df4["weight"], color=sns.color_palette()[8])
@@ -14,7 +14,13 @@ plt.ylabel("Count")
 plt.show()
 
 symptom = df1["Symptom_12"].value_counts()
-vis2 = plt.pie(symptom, labels=symptom.index, startangle=100, 
-               counterclock=False, wedgeprops={'width': 0.4})
+vis2 = plt.pie(symptom, labels=symptom.index, startangle=100, counterclock=False)
 plt.title("Symptom 12")
+plt.show()
+
+col = ['hotpink', 'gold', 'aqua', 'chartreuse']
+symptom = df1["Symptom_14"].value_counts()
+vis3 = plt.pie(symptom, labels=symptom.index, startangle=90,
+               counterclock=False, wedgeprops={'width': 0.4}, colors=col)
+plt.title("Symptom 14")
 plt.show()
